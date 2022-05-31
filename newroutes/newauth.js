59af68aa1219f9c7edeffca5db542dd8d06860d6
John2k19 <55826083+John2k19@users.noreply.github.com>
2022-06-01 00:03:55 +0800

    Add files via upload

---
+++ newroutes/newauth.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+
+router.post("/", async (req, res) => {
+	try {
+		const { error } = validate(req.body);
+		if (error)
+			return res.status(400).send({ message: error.details[0].message });
+
+		const user = await User.findOne({ email: req.body.email });
+
+		if (!user)
+			return res.status(401).send({ message: "Invalid Email or Password" });
+
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		if (!validPassword)
+			return res.status(401).send({ message: "Invalid Email or Password" });
+
+		const token = user.generateAuthToken();
+		const userData=user;
+
+		const tokenD= {token, userData};
+
+
+		res.status(200).send({ data: tokenD, message: "logged in successfully" });
+
+
+	} catch (error) {
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+});
+
+const validate = (data) => {
+	const schema = Joi.object({
+		email: Joi.string().email().required().label("Email"),
+		password: Joi.string().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+		console.log(jsonPayLoad)
+    return JSON.parse(jsonPayload);
+};
+
+
+
+
+router.get("/matchmaking", (req, res) => {
+	var rankVar = req.body;
+	var query = User.find().where('rankGroup').equals(rankVar).select('firstName email rankScore')
+  query.limit(9)
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+module.exports = router;
